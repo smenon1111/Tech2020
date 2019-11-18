7f97ee6f21bb76232400a5381075936a9b155310
smenon1111 <42555379+smenon1111@users.noreply.github.com>
2019-11-17 22:32:10 -0800

    good to go

---
+++ app/src/main/java/com/example/tech2020/Page2.java
@@ -0,0 +1,21 @@
+package com.example.tech2020;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class Page2 extends AppCompatActivity {
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, Page2.class);
+    };
+
+}
