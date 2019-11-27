c5b5fa5ce886e8f0e0b802fe305cdbc30750022c
smenon1111 <42555379+smenon1111@users.noreply.github.com>
2019-11-26 19:10:05 -0800

    Sophie's changes -home page + info page -see TODO's on how to link your pages to the buttons

--- app/src/main/java/com/example/tech2020/MainActivity.java
+++ app/src/main/java/com/example/tech2020/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.tech2020;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,15 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button locationButton = (Button) findViewById(R.id.button2);
+
         locationButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = Page2.makeIntent(MainActivity.this);
-                    startActivity(intent);
+                   openHome();
                 }
             });
 
     }
 
+    public void openHome() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
 
 }
